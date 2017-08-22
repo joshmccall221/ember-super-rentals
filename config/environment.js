1acc3da809caea5c40eb17da44147733ee713b60
Josh McCall <joshmccall221@gmail.com>
2017-08-22 15:15:22 -0500

    maps 2.0

--- config/environment.js
+++ config/environment.js
@@ -25,7 +25,7 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/super-rentals/';
+    ENV.rootURL = '/ember-super-rentals/';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
